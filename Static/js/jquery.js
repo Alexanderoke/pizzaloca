007eebf04d15463e3b973865e69faf949326e6ae
alexanderoke <alexander.oke@student.moringaschool.com>
2021-11-28 14:00:49 +0300

    order page re ordered

---
+++ Static/js/jquery.js
@@ -0,0 +1,16 @@
+// order page number of pizzas.
+$(document).ready(function(){
+  $(".chk").click(function(){
+    $(".input").show();
+     });
+
+  });
+  //   <br>
+  //  <input class="chk" type="checkbox"> 
+  //  <label class="form-check-label" >Large pizza</label>
+  //  <input class="input" type="number" placeholder="how many">
+  //  <br>
+  //  <label for="number">Medium</label>
+  //  <input type="number" placeholder="how many"><br>
+  //  <label for="number">Small</label>
+  //  <input type="number" placeholder="how many">
--- Static/js/main.js
+++ Static/js/main.js
@@ -1,5 +1,8 @@
-$(document).ready(function(){
-  $( function() {
-    $( "#dialog" ).dialog();
-  } );
-})
+function myFunction(){
+  var r = form("Press a button");
+  if (r == true) {
+      //your code to run
+  } else {
+      //code to run if cancel;
+  }
+}
