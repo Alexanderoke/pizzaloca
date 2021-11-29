eb508f3424f4a42686fd439cd8270271f5aca026
alexanderoke <alexander.oke@student.moringaschool.com>
2021-11-29 23:50:20 +0300

    javascript code modified

--- Static/js/main.js
+++ Static/js/main.js
@@ -1,7 +1,15 @@
-function totalOrder(){
-var x=document.getElementById("size").value;
+function yes(){
+  var yes=document.getElementById("yes");
+var no=document.getElementById("no");
+  if (yes) {
+    var x=document.getElementById("size").value;
 var y=document.getElementById("num").value;
 var z=document.getElementById("adress").value;
-
 alert("your order will be delivered at"+" "+z+" "+" "+"total price "+"ksh"+" "+x*y);
+  } else {
+    alert("Your order is ready for pick up")
+    
+  }
 }
+
+
