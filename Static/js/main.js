14f96b2cd8148c0091c6e4082ccf1da6c60da6a1
alexanderoke <alexander.oke@student.moringaschool.com>
2021-11-29 23:33:35 +0300

    place order button styled

--- Static/js/main.js
+++ Static/js/main.js
@@ -1,5 +1,7 @@
 function totalOrder(){
 var x=document.getElementById("size").value;
 var y=document.getElementById("num").value;
-alert("ksh"+" "+x*y);
+var z=document.getElementById("adress").value;
+
+alert("your order will be delivered at"+" "+z+" "+" "+"total price "+"ksh"+" "+x*y);
 }
