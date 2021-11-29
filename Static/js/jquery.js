9f8257278ecbbdc695706ec3d0d1c685e6a3bb8e
alexanderoke <alexander.oke@student.moringaschool.com>
2021-11-29 20:47:14 +0300

    order form added

--- Static/js/jquery.js
+++ Static/js/jquery.js
@@ -1,16 +1,50 @@
-// order page number of pizzas.
-$(document).ready(function(){
-  $(".chk").click(function(){
-    $(".input").show();
-     });
+// // order page number of pizzas.
+// $(function(){
+//   $(".large").clickr();
+//   
+// }); 
 
-  });
-  //   <br>
-  //  <input class="chk" type="checkbox"> 
-  //  <label class="form-check-label" >Large pizza</label>
-  //  <input class="input" type="number" placeholder="how many">
-  //  <br>
-  //  <label for="number">Medium</label>
-  //  <input type="number" placeholder="how many"><br>
-  //  <label for="number">Small</label>
-  //  <input type="number" placeholder="how many">
+
+
+
+
+
+
+
+
+
+
+// <h3>Topings available</h3>
+// <div class="row" >
+// <div class="col">
+// <p>pepperoni</p>
+// <img class="img-fluid img-thumbnail Pep" onclick="myFunction()" style="max-height: 300px; max-width:auto;" src="Static/images/pizza topings/Pepperoni-Pizza_7.jpg" alt="">
+    
+// </div>
+// <div class="col">
+// <p>BBQ Beef</p>
+// <img class="img-fluid img-thumbnail Pep" style="max-height: 300px; max-width:auto;" src="Static/images/pizza topings/BBQMeatlovers-1.jpg" alt="">
+// </div>
+// <div class="col">
+// <p>Supreme</p>
+// <img class="img-fluid img-thumbnail Pep" style="max-height: 300px; max-width:auto;" src="Static/images/pizza topings/Supreme-Pizza.jpg" alt="">
+// </div>
+// </div>
+// <div class="row">
+// <div class="col">
+// <p>hawaian</p>
+// <img class="img-fluid img-thumbnail Pep" style="max-height: 300px; max-width:auto;" src="Static/images/pizza topings/Ham-and-Pineapple.jpg" alt="">
+// </div>
+// <div class="col">
+// <p>Garlic Prawn and chilli</p>
+// <img class="img-fluid img-thumbnail Pep" style="max-height: 300px; max-width:auto;" src="Static/images/pizza topings/Prawn-Pizza_1.jpg" alt="">
+// </div>
+// <div class="col">
+// <p>Margherita</p>
+// <img class="img-fluid img-thumbnail Pep" style="max-height: 300px; max-width:auto;" src="Static/images/pizza topings/Margherita-pizza-overhead.jpg" alt="">
+// </div>
+// </div>
+// </div>
+
+
+  
--- Static/js/main.js
+++ Static/js/main.js
@@ -1,8 +1,3 @@
-function myFunction(){
-  var r = form("Press a button");
-  if (r == true) {
-      //your code to run
-  } else {
-      //code to run if cancel;
-  }
+function totalOrder(){
+var x=parseInt
 }
